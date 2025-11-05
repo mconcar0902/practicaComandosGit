f6594dab4d141a634f90f7c842108cd49d456423
marcos contreras <5marcoscontreras5@gmail.com>
2025-11-05 09:57:27 +0100

    Multiplicacion

--- src/es/comares/daw/ed/Calcuaradora.java
+++ src/es/comares/daw/ed/Calcuaradora.java
@@ -5,6 +5,7 @@ public class Calcuaradora {
 	public static void main(String[] args) {
 		System.out.println("1.Suma");
 		System.out.println("2.Resta");
+		System.out.println("3.Multiplicacion");
 	}
 
 }
